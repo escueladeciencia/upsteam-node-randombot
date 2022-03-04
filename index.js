f7ac30f2ead0c16f52d0883bbfb5a6878c46afa4
Raúl Ibáñez <raulibaper@gmail.com>
2022-03-04 20:37:27 +0100

    a bit of a comment + indentation

--- index.js
+++ index.js
@@ -3,35 +3,39 @@ const { Client, Intents, MessageEmbed } = require('discord.js');
 
 let entries = []
 
+// Itch.io´s Jam ID
+// Look for '<form method="post"' in HTML code
+// Source: https://itch.io/jam/upsteam-2
+// Output: action="/jam/277043/preview">
+const id = 277043;
+
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-  });
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
 
 client.once('ready', async (client) => {
 	console.log(`Ready! Logged in as ${client.user.tag}`);
-
-    try {
-        // Source: https://itch.io/jam/upsteam-2
-        // Extracted from <form method="post" action="/jam/277043/preview">
-        entries = await axios.get('https://itch.io/jam/277043/entries.json');
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const url = `https://itch.io/jam/${id}/entries.json`;
+    entries = await axios.get(url);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 client.on('messageCreate', async (message) => {
-    if (message.content === "/random") {
-        randomNumber = Math.floor(Math.random() * (entries.data.jam_games.length + 1));
-        entry = entries.data.jam_games[randomNumber];
+  if (message.content === "/random") {
+    randomNumber = Math.floor(Math.random() * (entries.data.jam_games.length + 1));
+    entry = entries.data.jam_games[randomNumber];
 
-        const exampleEmbed = new MessageEmbed()
-            .setTitle(entry.game.title)
-            .setURL(entry.game.url)
-            .setAuthor({ name: entry.game.user.name, iconURL: entry.game.cover, url: entry.game.user.url })
-            .setDescription(entry.game.short_text)
-            .setThumbnail(entry.game.cover);
-        await message.reply({ embeds: [exampleEmbed] });
-    }
-  });
+    const exampleEmbed = new MessageEmbed()
+      .setTitle(entry.game.title)
+      .setURL(entry.game.url)
+      .setAuthor({ name: entry.game.user.name, iconURL: entry.game.cover, url: entry.game.user.url })
+      .setDescription(entry.game.short_text)
+      .setThumbnail(entry.game.cover);
+    await message.reply({ embeds: [exampleEmbed] });
+  }
+});
 
 client.login(process.env.TOKEN);
