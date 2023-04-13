76e86a661a977bfa12962190a573ce551035af52
Raul Ibáñez <raulibaper@gmail.com>
2023-04-13 13:24:36 +0200

    Export adjusted to Up! Steam4 Itch attributes

--- utils/projects.js
+++ utils/projects.js
@@ -13,8 +13,9 @@ const csv = process.argv[2] === '--csv';
         for (const entry of entries.data.jam_games) {
             const entryItch = await axios.get(`https://itch.io${entry.url}`);
             const $ = cheerio.load(entryItch.data);
-            const centro = $(".field_responses").find("p:nth-child(1)").children("span").text();
-            const equipo = $(".field_responses").find("p:nth-child(2)").children("span").text();
+            const codigo = $(".field_responses").find("p:nth-child(1)").children("span").text();
+            const centro = $(".field_responses").find("p:nth-child(2)").children("span").text();
+            const equipo = $(".field_responses").find("p:nth-child(3)").children("span").text();
             const categoria = $(".field_responses").find(".badges").first().find("strong").text();
             const etapa = $(".field_responses").find(".badges").last().find("strong").text();
             let videoURL;
@@ -26,7 +27,7 @@ const csv = process.argv[2] === '--csv';
             } catch  {
               videoURL = "";
             }
-            const output = !csv ? `Centro: ${centro} - Equipo: ${equipo}` : `${entry.game.id};${centro};${equipo};${entry.game.title};${entry.game.cover};${entry.game.user.name};${entry.game.url};${entry.game.short_text};${categoria};${etapa};${videoURL};https://itch.io${entry.url};${entry.rating_count};${entry.coolness}`;
+            const output = !csv ? `Centro: ${centro} - Equipo: ${equipo}` : `${entry.game.id};${codigo};"${centro}";"${equipo}";"${entry.game.title}";${entry.game.cover};${entry.game.user.name};${entry.game.url};"${entry.game.short_text}";${categoria};"${etapa}";${videoURL};https://itch.io${entry.url};${entry.rating_count};${entry.coolness}`;
             console.log(output);
             centros.push(centro);
         }
